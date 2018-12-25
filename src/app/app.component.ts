a7e1b198967478d362ea28e9350805cf186d8884
Navneet Prakash <hello_navneet@hotmail.com>
2018-12-26 00:29:40 +0530

    Accesses reactive fornControls in the template, validates formControls,  and displays helpText or error messages.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.signupForm = new FormGroup({
-      username: new FormControl(null, Validators.required),
+      username: new FormControl(null, [Validators.required, Validators.pattern(/[a-z]/)]),
       email: new FormControl(null, [Validators.required, Validators.email]),
       gender: new FormControl("male")
     });
