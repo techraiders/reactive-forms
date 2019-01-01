80c04b931e6d2ae10d61edfaad615ba94963950d
Navneet Prakash <hello_navneet@hotmail.com>
2019-01-01 23:24:44 +0530

    Creates custom validarors.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,26 +9,49 @@ import { FormGroup, FormControl, Validators, FormArray } from "@angular/forms";
 export class AppComponent implements OnInit {
   genders = ["male", "female"];
   signupForm: FormGroup;
+  forbiddenUsernames = ['Cheris', 'Anna'];
 
   ngOnInit() {
-    this.signupForm = new FormGroup({
-      userData: new FormGroup({
-        username: new FormControl(null, [
-          Validators.required,
-          Validators.pattern(/[a-z]/)
-        ]),
-        email: new FormControl(null, [Validators.required, Validators.email])
-      }),
-      gender: new FormControl("male"),
-      hobbies: new FormArray([
+    try {
+      this.signupForm = new FormGroup({
+        userData: new FormGroup({
+          username: new FormControl(null, [
+            Validators.required,
+            Validators.pattern(/[a-z]/),
+            this.forbiddenName.bind(this)
+          ]),
+          email: new FormControl(null, [Validators.required, Validators.email])
+        }),
+        gender: new FormControl("male"),
+        hobbies: new FormArray([
 
-      ])
-    });
+        ])
+      });
+    } catch (err) {
+
+    }
   }
 
   onAddHobby () {
-    const control = new FormControl(null, Validators.required);
-    (<FormArray>this.signupForm.get('hobbies')).push(control);
+    try {
+      const control = new FormControl(null, Validators.required);
+      (<FormArray>this.signupForm.get('hobbies')).push(control);
+    } catch (err) {
+
+    }
+  }
+
+  forbiddenName (control: FormControl) : {[s: string]: boolean} {
+    try {
+      if (this.forbiddenUsernames.map(name => name.toLowerCase()).includes(control.value)) {
+        return {'nameIsForbidden': true};
+      } else {
+        /** Either return or nothing here, don't return false */
+        return null;
+      }
+    } catch (err) {
+
+    }
   }
 
   onSubmit() {
