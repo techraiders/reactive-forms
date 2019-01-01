ecfc12a5eb1f6760fc54e122faaa82315bc8273f
Navneet Prakash <hello_navneet@hotmail.com>
2019-01-01 22:59:03 +0530

    Dynamically adds reactive array of form controls.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { FormGroup, FormControl, Validators, FormArray } from "@angular/forms";
 
 @Component({
   selector: "app-root",
@@ -19,8 +19,16 @@ export class AppComponent implements OnInit {
         ]),
         email: new FormControl(null, [Validators.required, Validators.email])
       }),
-      gender: new FormControl("male")
+      gender: new FormControl("male"),
+      hobbies: new FormArray([
+
+      ])
     });
+  }
+
+  onAddHobby () {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
   }
 
   onSubmit() {
