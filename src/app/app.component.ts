a38d288147d60d60f24310ce5ecac152c7b6de61
Navneet Prakash <hello_navneet@hotmail.com>
2019-01-02 00:08:44 +0530

    Creates asynchronous validator, and validates email.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormControl, Validators, FormArray } from "@angular/forms";
+import { Observable } from "rxjs";
 
 @Component({
   selector: "app-root",
@@ -20,7 +21,7 @@ export class AppComponent implements OnInit {
             Validators.pattern(/[a-z]/),
             this.forbiddenName.bind(this)
           ]),
-          email: new FormControl(null, [Validators.required, Validators.email])
+          email: new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails)
         }),
         gender: new FormControl("male"),
         hobbies: new FormArray([
@@ -51,6 +52,23 @@ export class AppComponent implements OnInit {
       }
     } catch (err) {
 
+    }
+  }
+
+  forbiddenEmails (control: FormControl) : Promise<any> | Observable<any> {
+    try {
+      const promise = new Promise<any>((resolve, reject) => {
+        setTimeout(() => {
+          if (control.value === 'test@test.com') {
+            resolve({emailIsForbidden: true})
+          } else {
+            resolve(null);
+          }
+        }, 1500);
+      });
+      return promise;
+    } catch (err) {
+      
     }
   }
 
